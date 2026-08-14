a1057db721b8c553914985551c3e63b2cfbb9bb9
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-08-14 16:49:18 -0300

    atualizacao_loop

---
+++ desafios/Loops/exer2.py
@@ -0,0 +1,9 @@
+total = 0
+
+while True:
+    numero = int(input("Digite um número (ou 0 para sair): "))
+    if numero == 0:
+        break
+    total += numero
+
+print(f"A soma dos números digitados é: {total}")
